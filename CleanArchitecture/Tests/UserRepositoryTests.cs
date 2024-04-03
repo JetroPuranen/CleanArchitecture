4350e24da52747b39d2c8ab9414b8ae41d049baa
JetroPuranen <jetropuranenn@gmail.com>
2024-04-03 10:44:05 +0300

    Added userRepository tests

---
+++ CleanArchitecture/Tests/UserRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchitecture.Infrastructure.Data;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using CleanArchitecture.Domain.Entities;
+namespace CleanArchitecture.Tests
+{
+    public class UserRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public UserRepositoryTests()
+        {
+                _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+        }
+        [Fact]
+        public async Task AddAsync_ShouldAddUser_WhenUserIsValid()
+        {
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var userRepository = new UserRepository(context);
+                var user = new User { Name = "Test User", Email = "Test@email.com" };
+
+                await userRepository.Addsync(user);
+
+                var userInDb = await context.Users.FirstOrDefaultAsync(u => u.Email == "test@example.com");
+                Assert.NotNull(userInDb);
+                Assert.Equal("Test user", userInDb.Name);
+
+            }
+        }
+        [Fact]
+        public async Task EmailExistsAsync_ShouldReturnTrue_WhenEmailExists()
+        {
+            using (var context = new ApplicationDbContext(_options))
+            {
+                context.Users.Add(new User { Name = "Existing User", Email = "Existing@example.com" });
+                context.SaveChanges();
+
+                var userRepository = new UserRepository(context);
+
+                var exists = await userRepository.EmailExistsAsync("Existing@example.com");
+                Assert.True(exists);
+            }
+        }
+    }
+}
